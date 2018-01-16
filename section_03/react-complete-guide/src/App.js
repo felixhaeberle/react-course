7f2ae5c8f64d8d0edf8412491124303152bd9122
Felix Haeberle <f.haeberle@tojio.com>
2018-01-16 18:30:23 +0100

    update section 3 with create-react-app

--- section_03/react-complete-guide/src/App.js
+++ section_03/react-complete-guide/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hi, Im a React Developer</h1>
+        <p>This is really working.</p>
+        <Person name="Felix" age="22"/>
+        <Person name="Sven" age="23">My hobby is biking.</Person>
+        <Person name="David" age="21"/>
       </div>
     );
   }

9dc36574279738fb92df07db8f388e9017eff36d
Felix Haeberle <f.haeberle@tojio.com>
2018-01-16 18:59:22 +0100

    update section 3 with create-react-app customisation

--- section_03/react-complete-guide/src/App.js
+++ section_03/react-complete-guide/src/App.js
@@ -4,14 +4,43 @@ import Person from './Person/Person'
 
 
 class App extends Component {
+  state = {
+    persons : [
+      {name: "Felix", age: 22},
+      {name: "David", age: 28},
+      {name: "Sarah", age: 23}
+    ],
+    otherState: "an other state"
+  };
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons : [
+        {name: newName, age: 28},
+        {name: "Claus", age: 22},
+        {name: "Vivien", age: 25}
+      ]
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, Im a React Developer</h1>
         <p>This is really working.</p>
-        <Person name="Felix" age="22"/>
-        <Person name="Sven" age="23">My hobby is biking.</Person>
-        <Person name="David" age="21"/>
+        <button onClick={() => this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Louis")}
+        />
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }

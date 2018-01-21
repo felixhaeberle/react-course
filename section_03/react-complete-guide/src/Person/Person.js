01c28569b4da1e9b3cb2a1fba3909258006cd9cf
Felix Haeberle <f.haeberle@tojio.com>
2018-01-21 23:58:39 +0100

    Reverse changes

--- section_03/react-complete-guide/src/Person/Person.js
+++ section_03/react-complete-guide/src/Person/Person.js
@@ -4,10 +4,10 @@ import './Person.css';
 
 const person = (props) => {
   return (
-    <div className="Person littlePerson">
-      <p onClick={props.click}>Im {props.name} and Im {props.age}!</p>
+    <div className="Person">
+      <p onClick={props.click}>Im {props.name} and Im {props.age} !</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   )
 }

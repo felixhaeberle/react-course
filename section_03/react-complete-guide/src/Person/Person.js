8884724f29cd335e6edb83e8d82d279f876f0518
Felix Haeberle <f.haeberle@tojio.com>
2018-01-21 23:56:13 +0100

    Add class to JS file

--- section_03/react-complete-guide/src/Person/Person.js
+++ section_03/react-complete-guide/src/Person/Person.js
@@ -4,7 +4,7 @@ import './Person.css';
 
 const person = (props) => {
   return (
-    <div className="Person">
+    <div className="Person littlePerson">
       <p onClick={props.click}>Im {props.name} and Im {props.age} !</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />

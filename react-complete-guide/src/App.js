7d60ad11fb4b188dbe353fed0c25236df1747b15
Felix Haeberle <f.haeberle@tojio.com>
2018-01-22 22:18:35 +0100

    Adding dynamic input fields

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -15,14 +15,17 @@ class App extends Component {
     showPersons: false,
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons : [
-        {name: "Felix", age: 22},
-        {name: event.target.value, age: 28},
-        {name: "Sarah", age: 23}
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -55,7 +58,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id}/>
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
           })}
         </div>
       );

f4be6d6f81bd04a786c2ab4288c1d9ef86e99d82
Felix Haeberle <f.haeberle@tojio.com>
2018-01-22 22:48:30 +0100

    Adding hover styling to components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import './Person/Person.css';
+import Radium from 'radium';
 
 
 class App extends Component {
@@ -46,7 +47,11 @@ class App extends Component {
       color: 'white',
       border: '2px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -66,6 +71,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     let classes = [];
@@ -91,4 +100,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);

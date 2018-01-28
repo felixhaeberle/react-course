32ce54efc6ce1b4dc2749e3e78df53ae92acd9ef
Felix Haeberle <f.haeberle@tojio.com>
2018-01-29 00:36:47 +0100

    Focus on input when toggle button

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
+import PropTypes from 'prop-types';
 import classes from "./Person.css";
-
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
 
@@ -17,6 +17,9 @@ class Person extends Component {
 
 	componentDidMount() {
 		console.log('PersonJS inside componentDidMount');
+		if(this.props.position === 0) {
+			this.inputElement.focus();
+		}
 	}
 
 	render () {
@@ -26,10 +29,21 @@ class Person extends Component {
 			<Aux>
 				<p onClick={this.props.click}>Im {this.props.name} and Im {this.props.age} !</p>
 				<p>{this.props.children}</p>
-				<input type="text" onChange={this.props.changed} value={this.props.name} />
+				<input
+					ref={(inp) => { this.inputElement = inp }}
+					type="text"
+					onChange={this.props.changed}
+					value={this.props.name} />
 			</Aux>
 		);
 	}
+}
+
+Person.propTypes = {
+	click: PropTypes.func,
+	name: PropTypes.string,
+	age: PropTypes.number,
+	changed: PropTypes.func
 }
 
 export default withClass(Person, classes.Person);

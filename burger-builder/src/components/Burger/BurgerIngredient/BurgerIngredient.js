12556c7ec77627c15524ca4d076395324509292c
Felix Haeberle <f.haeberle@tojio.com>
2018-01-30 00:14:40 +0100

    Adding the burger with hardcoded ingredients

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -8,27 +8,27 @@ class BurgerIngredient extends Component {
   render () {
     let ingredient = null
 
-    switch (props.type) {
-        case ('bread-bottom')
+    switch (this.props.type) {
+        case ('bread-bottom'):
           ingredient = <div className={classes.BreadBottom}></div>;
           break;
-        case ('break-top')
+        case ('bread-top'):
           ingredient = (
             <div className={classes.BreadTop}>
               <div className={classes.Seeds1}></div>
               <div className={classes.Seeds2}></div>
             </div>);
           break;
-        case ('meat')
+        case ('meat'):
           ingredient = <div className={classes.Meat}></div>;
           break;
-        case ('cheese')
+        case ('cheese'):
           ingredient = <div className={classes.Cheese}></div>;
           break;
-        case ('salad')
+        case ('salad'):
           ingredient = <div className={classes.Salad}></div>;
           break;
-        case ('bacon')
+        case ('bacon'):
           ingredient = <div className={classes.Bacon}></div>;
           break;
         default: ingredient = null;
@@ -38,7 +38,7 @@ class BurgerIngredient extends Component {
 }
 
 BurgerIngredient.propTypes = {
-  type: PropTypes.string.isRequired;
+  type: PropTypes.string.isRequired
 }
 
 export default BurgerIngredient;

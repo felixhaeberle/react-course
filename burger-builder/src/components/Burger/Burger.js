5553e36bb5bb9ac9a00aa233efead6d03ffdbe68
Felix Haeberle <f.haeberle@tojio.com>
2018-01-30 00:26:32 +0100

    Adding state to the burger ingredient component

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -2,14 +2,18 @@ import React from 'react';
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-
-
 const burger = (props) => {
+  const tranformedIngredients = Object.keys(props.ingredients)
+      .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+          return <BurgerIngredient key={igKey + i} type={igKey} />
+        });
+      });
+
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      { tranformedIngredients }
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );

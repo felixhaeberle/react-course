9cfb2ec57dd6108387975cd9a7d13cc725493b36
Felix Haeberle <f.haeberle@tojio.com>
2018-02-05 23:40:20 +0100

    Adding Modal and Backdrop component

---
+++ burger-builder/src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const backdrop = () => (
+  <div>
+    {props.children}
+  </div>
+);
+
+export default backdrop;
---
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const modal = () => (
+  <div>
+    {props.children}
+  </div>
+);
+
+export default modal;

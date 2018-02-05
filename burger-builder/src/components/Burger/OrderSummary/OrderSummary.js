6048a1d95fa636fc3bd4ddd87a189df8c412d2e3
Felix Haeberle <f.haeberle@tojio.com>
2018-02-05 23:51:00 +0100

    Configure OrderSummary component

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const orderSummary = (props) => (
+import Aux from '../../../hoc/Aux';
 
-);
+const orderSummary = (props) => {
+  const ingredientSummary = props.ingredients
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+
+      </ul>
+    </Aux>
+  );
+};
 
 export default orderSummary;
